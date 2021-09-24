e6d622ef959d70e66ba7e2b59788317158b6c6e0
MisterX25 <Xavier.Carrel@cpnv.ch>
2021-09-24 14:21:08 +0200

    Publish sample

--- _icescrum_api.js
+++ _icescrum_api.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-var iceScrumToken = 'fff5c1aa2eb743e89684157897f47cab'
-projectIds = [
-    'ORDO2021',
-    'RUNEODESK2',
-    'LARASTA',
-    'HIXE',
-    'JOUTES2021',
-=======
 var iceScrumToken = '...' // Get it from IceScrum > MyAccount > Account > API tokens
 projectIds = [ // Get them from each project > Settings > Your Project > Key
     '...',
     '...'
->>>>>>> 630b3f1897fb911ae9d678729787153abf36a2eb
 ]
