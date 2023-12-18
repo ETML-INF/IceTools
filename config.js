170784b12127125449659b2dfeb265ba403fa524
XCarrel <Xavier.Carrel@cpnv.ch>
2023-12-18 18:10:29 +0100

    Move cartouche and IceScrum data to config.js file

---
+++ config.js
@@ -0,0 +1,15 @@
+const iceScrumToken = "e40edb162e7d479da7048b4066c9575b";
+const projects = [
+  { projectKey: "RASIOGAMIN", cartouche: {} },
+  { projectKey: "254QCM", cartouche: {} },
+  { projectKey: "254WHIDATH", cartouche: {} },
+  { projectKey: "254ALYFLOW", cartouche: {} },
+  { projectKey: "254ZAZACAK", cartouche: {} },
+  { projectKey: "I306HOUSET", cartouche: {} },
+  { projectKey: "I306BATIBE", cartouche: {} },
+  { projectKey: "I306GAMCOR", cartouche: {} },
+  { projectKey: "I306DIGIHO", cartouche: {} },
+  { projectKey: "I306CIMDOM", cartouche: {} },
+  { projectKey: "WOOXAVIER", cartouche: { Projet: "MyProject", Date: "Avril 1912" } }
+];
+const API_base_url = "https://etml.icescrum.com/ws/project/";
